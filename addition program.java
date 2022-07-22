bf5944f042a02db6448096d9a65945b585f37fa0
uchiaxitachi <chirag794rawat@gmail.com>
2022-07-22 19:00:39 +0530

    Addition in java

---
+++ addition program.java
@@ -0,0 +1,19 @@
+class main{
+    /**
+     * @param args
+     */
+     public static void main(String[] args) {
+        
+        System.out.println("enter two numbers");
+        int first = 10;
+        int second =20;
+
+        System.out.println(first+" "+ second);
+
+        int sum = first+second;
+        System.out.println("the sum is: " + sum);
+
+    }
+
+}
+
