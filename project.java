e5bb6cf7f7ab2d7d63cdbd2afcae1fee3fec6bb9
uchiaxitachi <chirag794rawat@gmail.com>
2022-08-02 11:14:58 +0530

    string in java

---
+++ project.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class project {
+    public static void main(String[] args){
+    //String name=new String("harry");
+   /* String name="harry  ";
+        System.out.print("the name is :");
+        System.out.print(name);
+        int a=67326;
+        float b=65430.87f;
+        System.out.printf("the value of a is %d and value of b is %f",a,b);*/
+Scanner sc=new Scanner(System.in);
+String st=sc.nextLine();
+        System.out.println(st);
+
+    }
+}
